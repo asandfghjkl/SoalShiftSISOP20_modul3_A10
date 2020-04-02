d81e8ba5ee068c51f988108de2d3a016a1ec469e
Adrian Danindra <60964359+Bluejake3@users.noreply.github.com>
2020-04-03 05:34:16 +0700

    Add files via upload

---
+++ soal4/4b.c
@@ -0,0 +1,60 @@
+#include<stdio.h> 
+#include<pthread.h> 
+#include<unistd.h> 
+#include<stdlib.h> 
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+void *sum(void* arg)
+{
+	int n=(int *)arg;
+	int *x=(int*)malloc(sizeof(int));	
+	*x=(n*(n+1))/2;
+	pthread_exit(x);
+}
+  
+int main()
+{
+	key_t key = 1234;
+	int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
+	int *pass;pass=shmat(shmid, NULL, 0);
+	int i;
+	int matrix[20];
+	
+	for(i=0;i<20;i++) matrix[i]=pass[i];
+	shmdt(pass);
+	for(i=0;i<20;i++)
+	{
+		printf("%d ", matrix[i]);
+		if ((i + 1) % 5 == 0) 
+		printf("\n");
+	}
+	
+	
+
+	pthread_t threads[20]; 
+
+	int count=0;
+	int current;
+
+
+	for (i = 0; i < 20; i++)  
+	{ 
+		current=matrix[i];
+		pthread_create(&threads[count++], NULL,sum, (void*)(current)); 
+
+	}
+	
+	for (i = 0; i < 20; i++)  
+	{ 
+	void *k; 
+        
+
+	pthread_join(threads[i], &k); 
+                         
+	int *p = (int *)k; 
+	printf("%d ",*p); 
+	if ((i + 1) % 5 == 0) 
+		printf("\n"); 
+    } 
+}
