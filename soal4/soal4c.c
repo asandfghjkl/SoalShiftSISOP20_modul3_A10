588ac2476b35d721056d60160308fe8e63a0474c
asandfghjkl <57932162+asandfghjkl@users.noreply.github.com>
2020-04-03 10:15:54 +0700

    Update soal4c.c

--- soal4/soal4c.c
+++ soal4/soal4c.c
@@ -1,54 +1,37 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<limits.h>
-#include<dirent.h>
-#include<sys/types.h> 
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
-int main(){
-	char cwd[PATH_MAX];
-	struct dirent *de;
-	getcwd(cwd, sizeof(cwd));
-	DIR *dr = opendir(cwd);
-	int p[2], i; 
-	if (pipe(p) < 0) 
-		exit(1);
-	char pass[100];
-	char stop[]="stop";
-	pid_t child_id;
-	child_id=fork();
+int main() {
+    	int fd[2];
 
-	if (child_id>0)
-	{
-		close(p[0]);
-		while((de=readdir(dr))!=NULL)
-		{
-			strcpy(pass,de->d_name);
-			if (pass[0]!='.')
-			write(p[1],pass,100);
+    	pid_t pid;
+    	int status;
 
-		}
-		write(p[1],stop,strlen(stop)+1);
-		close(p[1]);
-	}
-	else
-	{
-		close(p[1]);
-		char buffer[100];
-		int count=0,i=0;
-		while(strcmp(buffer,stop))
-		{
-			read(p[0], buffer, 100);
-			count++;
-	
-		}
-		
-		printf("%d\n",count-1);
-		close(p[0]);
+	pipe(fd);
+	pid = fork();
 
-	}
-	
+    	if(pid < 0) {
+        	exit(EXIT_FAILURE);
+    	}
 
+    	if(pid == 0) {
+        	dup2(fd[1], 1);
+        	close(fd[0]);
+        	close(fd[1]);
+        	char *argv[] = {"ls", NULL};
+        	execv("/bin/ls", argv);
+    	}
+    	else {
+        	while(wait(&status) > 0);
 
+            	dup2(fd[0], 0);
+            	close(fd[0]);
+            	close(fd[1]);
+            	char *argv[] = {"wc", "-l", NULL};
+            	execv("/usr/bin/wc", argv);
+    	}
+    	return 0;
 }
